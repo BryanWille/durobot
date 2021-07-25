01e60e239509e5e9dfbdeae56a9a3a9a3ed68f02
Bryan <bryanelliw@gmail.com>
2021-07-25 17:22:58 -0300

    index.html

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const Discord = require("discord.js");
-const {prefix, token} = require("./config.json");
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
 
-client.on('message', message =>{
-  const userName = message.author.username;
-  if(message.content.startsWith(`!play`)){
-    if(userName == 'ElliW'){
-      message.reply("Isso sim é música boa!");
-    } else if(userName == 'luan123'){
-      message.reply('**Vai por funk na pqp**');
-    } else if(userName == 'gomide17'){
-      message.reply('**Sério? não cansa de ouvir a poha dessa banda**');
-    } else {
-      message.reply('Puta música bosta');
-    }
-  } else if (message.content === `${prefix}servidor`){
-    message.reply(`Servidor **${message.guild.name}** criado em: ${message.guild.createdAt}`)
-  }
-})
+client.on('message', message => {
+	const userName = message.author.username;
+	if(userName == 'Eliass') {
+		message.reply('Calaboca não te perguntei nada!');
+	}
+	if (message.content.startsWith('!play')) {
+		if (userName == 'ElliW') {
+			message.reply('Isso sim é música boa!');
+		}
+		else if (userName == 'Luan123') {
+			message.reply('**Vai por funk na pqp**');
+		}
+		else if (userName == 'gomide17' && (message.content == 'milky' || message.content == 'chance')) {
+			message.reply('**Sério Adriano não vai parar de ouvir a porra dessa banda nunca**');
+		}
+		else {
+			message.reply('Puta música bosta');
+		}
+	}
+	else if (message.content === `${prefix}servidor`) {
+		message.reply(`Servidor **${message.guild.name}** criado em: ${message.guild.createdAt}`);
+	}
+});
 
 client.login(token);
 
